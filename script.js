58aa7d57da5c12e0918eeda45fe5c7efa73023c5
nickdowski <nickdowski@gmail.com>
2025-03-22 11:54:54 -0500

    Create a check for winner conditional

--- script.js
+++ script.js
@@ -20,30 +20,34 @@ let computerScore = 0;
 function playRound(humanChoice, computerChoice) {
     // Make humanChoice case insensitive
     humanChoice = humanChoice.toLowerCase();
-    // Display the choices and determine the winner
+    // Display the choices and determine the winner of round
     if (humanChoice === computerChoice) {
-        alert('Tie!');
     } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
-        alert('You Win!');
         humanScore++;
     } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
-        alert('You Win!');
         humanScore++;
     } else if (humanChoice === 'paper' && computerChoice === 'rock') {
-        alert('You Win!');
         humanScore++;
     } else {
-        alert('You Lose!');
         computerScore++;
     }
     
-    // Display the scores
+    // Update the scores
     const humanScoreDisplay = document.getElementById('human-score');
     humanScoreDisplay.textContent = humanScore;
     const computerScoreDisplay = document.getElementById('computer-score');
     computerScoreDisplay.textContent = computerScore;
 
+    // Check for winner of game
+    if (humanScore === 5) {
+        alert('You Win!');
+    } else if (computerScore === 5) {
+        alert('You Lost!');
+    }
 }
+
+
+
 
 // Create a rock button
 const rock = document.createElement('button');
